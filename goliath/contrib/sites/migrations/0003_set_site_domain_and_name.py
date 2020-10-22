b96d028534797aa8d5afb45b1ad87760bea95894
Johannes Filter <hi@jfilter.de>
2020-10-22 16:29:22 +0200

    adapt URL settings

--- goliath/contrib/sites/migrations/0003_set_site_domain_and_name.py
+++ goliath/contrib/sites/migrations/0003_set_site_domain_and_name.py
@@ -13,7 +13,7 @@ def update_site_forward(apps, schema_editor):
     Site.objects.update_or_create(
         id=settings.SITE_ID,
         defaults={
-            "domain": "goliath.lab1.algorithmwatch.org",
+            "domain": "lab2.algorithmwatch.org",
             "name": "Goliath",
         },
     )
