6bbd36d73a5ed9b47edff65860e7abd5488f001a
Johannes Filter <hi@jfilter.de>
2020-10-26 20:59:57 +0100

    improve basic HTML layout + localize urls

--- goliath/casehandling/urls.py
+++ goliath/casehandling/urls.py
@@ -4,8 +4,9 @@ from django.urls import include, re_path
 from .views import case_type_list_view, case_create_view, case_detail_view
 
 urlpatterns = [
-    path("new/", view=case_type_list_view),
-    path("new/<int:case_type>/", view=case_create_view),
-    path("case/<int:pk>/", view=case_detail_view),
+    path("neu/", view=case_type_list_view, name="new"),
+    path("neu/<int:case_type>/", view=case_create_view),
+    path("anliegen/<int:pk>/", view=case_detail_view),
+    path("anliegen/", view=case_type_list_view, name="cases"),
     re_path(r"^anymail/", include("anymail.urls")),
 ]
