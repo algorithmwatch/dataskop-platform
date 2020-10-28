cebfd1684bbed9a5141f125895a639b7f9b25fb9
Johannes Filter <hi@jfilter.de>
2020-10-28 15:30:46 +0100

    make highlighting in nav work for wizzard

--- goliath/casehandling/urls.py
+++ goliath/casehandling/urls.py
@@ -5,7 +5,7 @@ from .views import case_type_list_view, case_create_view, case_detail_view
 
 urlpatterns = [
     path("neu/", view=case_type_list_view, name="new"),
-    path("neu/<int:case_type>/", view=case_create_view),
+    path("neu/<int:case_type>/", view=case_create_view, name="new-wizzard"),
     path("anliegen/<int:pk>/", view=case_detail_view),
     path("anliegen/", view=case_type_list_view, name="cases"),
     re_path(r"^anymail/", include("anymail.urls")),
