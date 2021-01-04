532e9feee2a373559424d575ff1236c0202ab694
Johannes Filter <hi@jfilter.de>
2021-01-05 00:00:44 +0100

    fix broken registration

--- config/urls.py
+++ config/urls.py
@@ -7,18 +7,18 @@ from django.views.generic import TemplateView
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-
     path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
     path(
-        "ueber-uns/", TemplateView.as_view(template_name="pages/about.html"), name="about"
+        "ueber-uns/",
+        TemplateView.as_view(template_name="pages/about.html"),
+        name="about",
     ),
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
     # User management
-    # path("user/", include("goliath.users.urls", namespace="users")),
+    path("user/", include("goliath.users.urls", namespace="users")),
     path("account/", include("allauth.urls")),
     path("", include("goliath.casehandling.urls")),
-
     # Your stuff: custom urls includes go here
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
