03bb613c4ccbb341f561391ba097707985843762
Gregor Weichbrodt <gregorweichbrodt@gmail.com>
2021-02-03 12:47:12 +0100

    add interfont; tailwint config WIP

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,12 @@
 module.exports = {
-  purge: [],
+  purge: {
+    content: [
+      './goliath/templates/**/*.html',
+    ],
+
+    // These options are passed through directly to PurgeCSS
+    options: {}
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
