75e508e40e89044022c756f10fe5ffa95603492e
Gregor Weichbrodt <gregorweichbrodt@gmail.com>
2021-02-03 16:01:46 +0100

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,42 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+
+      black: '#111111',
+      white: '#fcfcfc',
+      // primary: colors.indigo['700'],
+
+      gray: {
+        100: '#e9e9e9',
+        200: '#d3d3d3',
+        300: '#bdbdbd',
+        400: '#a7a7a7',
+        500: '#919191',
+        600: '#7a7a7a',
+        700: '#646464',
+        800: '#4e4e4e',
+        900: '#383838',
+      },
+    },
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+      serif: ['Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
+      mono: ['ui-monospace','SFMono-Regular','Menlo','Monaco','Consolas','"Liberation Mono"','"Courier New"','monospace'],
+    },
+    fontWeight: {
+      // thin: '100',
+      // extralight: '200',
+      // light: '300',
+      normal: '400',
+      medium: '500',
+      semibold: '600',
+      bold: '700',
+      // extrabold: '800',
+      // black: '900',
+    },
     extend: {},
   },
   variants: {
