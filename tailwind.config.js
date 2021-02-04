809ccbeb036a99fb9610f332c88930f53074c70f
Gregor Weichbrodt <gregorweichbrodt@gmail.com>
2021-02-04 16:37:52 +0000

    add webpack; etc.

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,13 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      // '2xl': '1536px',
+    },
     colors: {
       transparent: 'transparent',
       current: 'currentColor',
