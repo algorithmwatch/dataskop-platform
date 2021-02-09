37bdb2c8deb73952039081708d8fef27dacd1ccb
Johannes Filter <hi@jfilter.de>
2021-02-09 21:20:31 +0100

    add factory to create ongoing case

--- goliath/casehandling/tests/factories.py
+++ goliath/casehandling/tests/factories.py
@@ -1,7 +1,11 @@
+import random
+
 import factory
 from factory.django import DjangoModelFactory
 
-from ..models import Case, CaseType, Entity, ReceivedMessage, SentMessage
+from goliath.users.tests.factories import UserFactory
+
+from ..models import Case, CaseType, Entity, ReceivedMessage, SentMessage, Status
 
 
 class EntityFactory(DjangoModelFactory):
@@ -23,6 +27,21 @@ class CaseTypeFactory(DjangoModelFactory):
     questions = "{}"
 
 
+class CaseTypeFactoryWithEntitiesFactory(CaseTypeFactory):
+    @factory.post_generation
+    def entities(self, create, extracted: int = 3, **kwargs):
+        """
+        called like CaseTypeFactoryWithEntitiesFactory(entities=4)
+        """
+        if not create:
+            # Build, not create related
+            return
+
+        if extracted:
+            for _ in range(extracted):
+                self.entities.add(EntityFactory())
+
+
 class CaseFactory(DjangoModelFactory):
     class Meta:
         model = Case
@@ -31,6 +50,40 @@ class CaseFactory(DjangoModelFactory):
     email = factory.Faker("company_email", locale="de")
     questions = "{}"
     answers = "{}"
+
+
+class OngoingCaseFactory(CaseFactory):
+    user = factory.SubFactory(UserFactory)
+    status = factory.LazyAttribute(
+        lambda o: random.choice(Status.choices)[0]
+        if o.force_status is None
+        else o.force_status
+    )
+
+    class Params:
+        force_status = None
+
+    @factory.post_generation
+    def ongoing(self, create, extracted: int = 3, **kwargs):
+        """
+        called like CaseTypeFactoryWithEntitiesFactory(entities=4)
+        """
+        if not create:
+            # Build, not create related
+            return
+
+        self.save()
+
+        if extracted:
+            self.case_type = CaseTypeFactoryWithEntitiesFactory(entities=extracted)
+            self.selected_entities.add(*self.case_type.entities.all())
+
+            SentMessageFactory(case=self)
+            for _ in range(extracted):
+                if random.choice([True, False]):
+                    SentMessageFactory(case=self)
+                else:
+                    ReceivedMessageFactory(case=self)
 
 
 class SentMessageFactory(DjangoModelFactory):
---
+++ goliath/casehandling/tests/test_factories.py
@@ -0,0 +1,26 @@
+import pytest
+
+from ..models import Status
+from .factories import CaseTypeFactoryWithEntitiesFactory, OngoingCaseFactory
+
+pytestmark = pytest.mark.django_db
+
+
+def test_factories():
+    cte = CaseTypeFactoryWithEntitiesFactory(entities=4)
+    assert cte.entities.all().count() == 4
+
+    cte2 = CaseTypeFactoryWithEntitiesFactory(entities=3)
+    assert cte2.entities.all().count() == 3
+
+    c = OngoingCaseFactory(ongoing=4)
+    assert len(c.all_messages()) == 5
+    assert c.user.email is not None
+
+
+def test_factories_status():
+    ctes = OngoingCaseFactory.create_batch(
+        20, ongoing=4, force_status=Status.WAITING_USER_INPUT
+    )
+    for c in ctes:
+        assert c.status == Status.WAITING_USER_INPUT
