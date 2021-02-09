aab57f07c1de48ec4af160a7091af7728d8b66ec
Johannes Filter <hi@jfilter.de>
2021-02-09 23:38:29 +0100

    save survey state to locale storage

--- goliath/src/js/wizard.js
+++ goliath/src/js/wizard.js
@@ -1,9 +1,8 @@
-import * as $ from 'jquery';
+import * as $ from "jquery";
 import * as Survey from "survey-jquery";
 
 // export for others scripts to use
 window.$ = $;
-
 
 function addUserToJson(surveyJSON) {
   var lastPageIndex = surveyJSON.pages.length - 1;
@@ -50,6 +49,29 @@ function addUserToJson(surveyJSON) {
   return surveyJSON;
 }
 
+// via https://surveyjs.io/Examples/Library?id=survey-editprevious&platform=jQuery&theme=modern#content-js
+
+var storageName = "aw-goliath-storage";
+
+function loadState(survey) {
+  var storageSt = window.localStorage.getItem(storageName) || "";
+  var res = {};
+  if (storageSt) res = JSON.parse(storageSt);
+
+  if (res.data) survey.data = res.data;
+}
+
+function saveState(survey) {
+  var res = {
+    data: survey.data,
+  };
+  window.localStorage.setItem(storageName, JSON.stringify(res));
+}
+
+function removeState() {
+  window.localStorage.removeItem(storageName);
+}
+
 function setupSurvey(casetypeId, surveyJSON, csrfToken, newUser) {
   if (newUser) surveyJSON = addUserToJson(surveyJSON);
 
@@ -69,6 +91,7 @@ function setupSurvey(casetypeId, surveyJSON, csrfToken, newUser) {
 
     $.post("/neu/" + casetypeId + "/", body)
       .done(function (successData) {
+        removeState();
         window.location.replace(successData.url);
       })
       .fail(function () {
@@ -115,7 +138,7 @@ function setupSurvey(casetypeId, surveyJSON, csrfToken, newUser) {
     return true;
   }
 
-  var surveyValueChanged = function (sender, options) {
+  function surveyValueChanged(sender, options) {
     if (options.name != "previewhtml" && window.isCompleting === false) {
       window.finalText = constructLetterText();
       window.awsurvey.getQuestionByName("previewhtml").html =
@@ -129,39 +152,31 @@ function setupSurvey(casetypeId, surveyJSON, csrfToken, newUser) {
     if (el) {
       el.value = options.value;
     }
-  };
+  }
 
-  // not using default theme right now
-
-  // var defaultThemeColors = Survey.StylesManager.ThemeColors["default"];
-  // defaultThemeColors["$main-color"] = "#1f9bcc";
-  // defaultThemeColors["$main-hover-color"] = "#1f9bcc";
-  // defaultThemeColors["$text-color"] = "#4a4a4a";
-  // defaultThemeColors["$header-color"] = "#7ff07f";
-
-  // defaultThemeColors["$header-background-color"] = "#4a4a4a";
-  // defaultThemeColors["$body-container-background-color"] = "#f8f8f8";
-
-  // Survey.StylesManager.applyTheme();
-
+  // setting up the survey & setting some appropiate values
   var survey = new Survey.Model(surveyJSON);
-
   survey.locale = "de";
-  // survey.showProgressBar = "top";
-  // survey.showPageNumbers = true;
-  // survey.showTitle = false;
   survey.showPreviewBeforeComplete = true;
   survey.completedHtml = "<p>Bitte einen kurzen Augenblick warten...</p>";
+  survey.completeText = "Anliegen melden & versenden";
 
   // https://surveyjs.io/Documentation/Library?id=SurveyModel#questionsOnPageMode
   survey.questionsOnPageMode = "singlePage";
 
-  // ensure deleting all values when
+  // ensure deleting all values when changing a value further below
   survey.clearInvisibleValues = "onHidden";
 
-  survey.completeText = "Anliegen melden & versenden";
+  // persist state every time a new question gets rendered
+  survey.onAfterRenderQuestion.add(function (survey, options) {
+    saveState(survey);
+  });
 
   window.awsurvey = survey;
+
+  // load the initial state if available
+  // TODO: if the survey was almost complete, the previewtext + button are not shown
+  loadState(window.awsurvey);
 
   // what classes to customize
   // https://surveyjs.io/Examples/Library/?id=survey-customcss&platform=jQuery&theme=modern#content-docs
