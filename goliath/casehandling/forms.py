117c5506ed8dd56e3952d0b5837592e9ca00fe2e
Johannes Filter <hi@jfilter.de>
2021-02-16 20:56:50 +0100

    ensure unique values

--- goliath/casehandling/forms.py
+++ goliath/casehandling/forms.py
@@ -24,14 +24,16 @@ def get_admin_form_preview(ct):
         r"\{\%(.*?)\%\}", ct.letter_template
     )
     context_variables = [
-        x.strip()
+        x.strip().lower()
         for x in context_variables
-        if x.strip() not in ["if", "else", "endif", "for", "endfor"]
+        if x.strip().lower() not in ["if", "else", "endif", "for", "endfor"]
     ]
 
     # remove all expressions, only keep variables
     context_variables = [x.split("==")[0].split("!=")[0] for x in context_variables]
-    context_variables = [re.sub(r"^(if|for) ", "", x) for x in context_variables]
+    context_variables = [
+        re.sub(r"^(if|for) ", "", x).strip() for x in context_variables
+    ]
 
     # make unique
     for var in set(context_variables):
