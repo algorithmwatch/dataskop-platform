15b20c89a4a81066f3e20268cc5eaccecf242c8a
Gregor Weichbrodt <gregorweichbrodt@gmail.com>
2021-02-22 14:54:07 +0000

    update donor container position

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,7 @@ module.exports = {
       md: '768px',
       lg: '1024px',
       xl: '1280px',
-      // '2xl': '1536px',
+      '2xl': '1400x',
     },
     colors: {
       transparent: 'transparent',
