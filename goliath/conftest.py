c7a6cb222d8719c10c61782e972fbd7955f8d3e9
Johannes Filter <hi@jfilter.de>
2021-04-28 13:39:30 +0200

    fix + improve user related tests

--- goliath/conftest.py
+++ goliath/conftest.py
@@ -1,8 +1,9 @@
 import pytest
+from django.contrib.auth import get_user_model
 
-from goliath.users.models import User
 from goliath.users.tests.factories import UserFactory
 
+User = get_user_model()
 
 @pytest.fixture(autouse=True)
 def media_storage(settings, tmpdir):
