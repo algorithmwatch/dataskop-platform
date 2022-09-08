ec2790cc0b6a084ce372305bc7f5b1b3cc8a1487
Johannes Filter <hi@jfilter.de>
2022-09-08 10:57:32 +0200

    Fix middle ware

--- dataskop/utils/middleware.py
+++ dataskop/utils/middleware.py
@@ -3,7 +3,19 @@ from dataskop.campaigns.tasks import add_event
 
 class EventExceptionsMiddelware:
     def __init__(self, get_response):
-        pass
+        self.get_response = get_response
+        # One-time configuration and initialization.
+
+    def __call__(self, request):
+        # Code to be executed for each request before
+        # the view (and later middleware) are called.
+
+        response = self.get_response(request)
+
+        # Code to be executed for each request/response after
+        # the view is called.
+
+        return response
 
     def process_exception(self, request, exception):
         add_event.delay(
